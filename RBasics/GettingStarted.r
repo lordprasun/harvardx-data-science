32d19af943b01c01bb41c1cd1c0c4f41f8b0a3f7
Prasun Chakraborty <lordprasun@gmail.com>
2018-12-13 00:15:19 +0530

    Basics,Inbuilt Functions, Data types,vectors and sorting

--- RBasics/GettingStarted.r
+++ RBasics/GettingStarted.r
@@ -46,3 +46,8 @@ table(murders$region)
 sort(murders$total)
 index <- order(murders$total)
 murders$total[index]
+
+# Store the per 100,000 murder rate for each state in murder_rate
+murder_rate <- murders$total/murders$population * 100000
+# Calculate the average murder rate in the US 
+mean(murder_rate)
--- RBasics/vectors.r
+++ RBasics/vectors.r
@@ -37,3 +37,21 @@ x <- c(1, 3, 5,"a")
 # Typecast the vector to get an integer vector
 # You will get a warning but that is ok
 x <- as.numeric(x)
+
+
+## ARITHMATIC
+
+# Assign city names to `city` 
+city <- c("Beijing", "Lagos", "Paris", "Rio de Janeiro", "San Juan", "Toronto")
+# Store temperature values in `temp`
+temp <- c(35, 88, 42, 84, 81, 30)
+
+# Convert temperature into Celsius and overwrite the original values of 'temp' with these Celsius values
+temp <- (5/9)*(temp-32)
+# Create a data frame `city_temps` 
+city_temps <- data.frame(city,temp)
+
+# Define an object `x` with the numbers 1 through 100
+x <- 1:100
+# Compute the sum 
+sum(1/x^2)
